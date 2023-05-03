c9b51b67d943d5e813a805d11dfc032803f37e25
lola Seoane <lola.seoane@advnz.de>
2023-05-03 17:24:14 +0200

    Styles and translate

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -10,20 +10,27 @@ export class CarouselComponent {
   series:any[]=[
 
     {
-      name:'Barra',
-      img: './../assets/img/Carrusel/2.jpg',
+      name:'Menjador Principal',
+      img: './../assets/img/Carrusel/9.png',
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
     },
 
-    { name:'Comedor 2',
-      img: './../assets/img/Carrusel/9.png',
+    { name:'Barra',
+      img: './../assets/img/Carrusel/2.jpg',
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
+    },
+    {
+      name:'Celler',
+      img: './../assets/img/Carrusel/11.jpg',
+      desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
+
     },
     {
       name:'Antiga Fonda',
       img: './../assets/img/Carrusel/1.jpg',
       desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat'
 
-    },
+    }
+
   ]
 }
